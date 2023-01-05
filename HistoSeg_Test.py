26e5aba6ce872c26d1d13a759bfc54c9de149aeb
Ved Thakur <thakur.6@iitj.ac.in>
2023-01-06 02:16:04 +0530

    Update HistoSeg_Test.py

--- HistoSeg_Test.py
+++ HistoSeg_Test.py
@@ -501,7 +501,7 @@ def preprocess_input(x):
 from tensorflow.keras import backend as K
 
 def dice_coef(y_true, y_pred, smooth=1):
-
+    y_true = tf.cast(y_true, tf.float32)	
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
 
